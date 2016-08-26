8fb4a4002726755953ea2531a0f634ee6371adc6
Prairie8585 <ivankurysh7532@gmail.com>
2016-08-27 00:01:43 +0300

    init

--- src/in/MyServlet.java
+++ src/in/MyServlet.java
@@ -2,12 +2,23 @@ package in;
 
 import java.io.*; 
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class MyServlet extends GenericServlet {
-public void service(ServletRequest request, ServletResponse response) 
-		throws ServletException, IOException { 
-	response.setContentType("text/html"); 
-	PrintWriter pw = response.getWriter(); 
-	pw.println("<B>Hello!"); 
-	pw.close(); }
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		System.out.println("Hello World!)");
+		
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("Hello from servlet");
+		out.close();
+	}
+	
 }
